07877de7b8d73d14721ebf6948aa82ac77e3eb04
djamelmkr <162751166+djamelmkr@users.noreply.github.com>
2024-03-31 02:23:00 +0100

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -2,50 +2,54 @@ import * as React from "react";
 import { StyleSheet, View, Text, Pressable } from "react-native";
 import { Image } from "expo-image";
 import { useNavigation } from "@react-navigation/native";
-import { Color, Padding, Border, FontFamily, FontSize } from "../GlobalStyles";
+import TextBox from 'react-native-password-eye';
 import { TextInput } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import ForgotPwd from "./ForgotPwd";
 
 const Login = () => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.loginpage}>
-      <Text style={styles.login1}>Login</Text>
-      <View style={[styles.input]}>
+      <Text style={styles.login}>Login</Text>
+      <View style={[styles.container]}>
         <Image
           style={styles.iconlylightlock}
           contentFit="cover"
           source={require("../assets/stylestroke.png")}
         />
-        <TextInput placeholder="Email" />
+        <TextInput placeholder="Email"
+             placeholderTextColor="#7c7c7c" 
+        style={{width:"100%"}} />
       </View>
-      <View style={[styles.input]}>
+      <View style={[styles.container]}>
         <Image
           style={styles.iconlylightlock}
           contentFit="cover"
           source={require("../assets/iconlylightlock.png")}
         />
-        <TextInput placeholder="Password" />
+        <TextBox onChangeText={(text) => console.log('onChangeText: ', text)} 
+       containerStyles={{width:"88%",}}
+      secureTextEntry={true}
+      eyeColor="#7c7c7c"
+      placeholder="Password"
+      placeholderTextColor="#7c7c7c" 
+      />
       </View>
       <Pressable  onPress={() => navigation.navigate("ForgotPwd")}>
         <Text style={styles.forgotPassword}>Forgot Password?</Text>
       </Pressable>
       <Pressable
-        style={[styles.buttonfirst, styles.buttonfirstFlexBox]}
+        style={[styles.buttonfirst]}
         onPress={() => navigation.navigate("MonProfil")}
       >
-        <Text style={[styles.signUp, styles.textTypo]}>Login</Text>
+        <Text style={[ styles.textTypo]}>Login</Text>
       </Pressable>
       <Pressable
-        style={styles.dontYouHaveContainer}
         onPress={() => navigation.navigate("SignUp")}
       >
-        <Text style={styles.text1}>
-          <Text style={styles.dontYouHave}>Don't you have an account? </Text>
-
-          <Text style={styles.signUp1}>Sign Up</Text>
+        <Text style={styles.text}>
+          <Text style={styles.dontYouHave}>Don't you have an account?  </Text>
+          <Text style={styles.log}>Sign Up</Text>
         </Text>
       </Pressable>
     </View>
@@ -53,64 +57,19 @@ const Login = () => {
 };
 
 const styles = StyleSheet.create({
-  loginFlexBox: {
-    justifyContent: "space-between",
-    backgroundColor: Color.neutralWhite,
-  },
-  textPosition: {
-    left: "0%",
-    top: "0%",
-    height: "100%",
-    position: "absolute",
-    width: "100%",
-  },
-  inputShadowBox: {},
   textTypo: {
-    fontFamily: FontFamily.subheadLgSHLgMedium,
-    fontWeight: "500",
-    fontSize: FontSize.subheadLgSHLgMedium_size,
+    color:"#FFFFFF",
+    fontFamily:"Poppins-Medium",
+    fontSize: 16,
   },
-  numberTypo: {
-    height: 19,
-    textAlign: "left",
-    fontFamily: FontFamily.nunitoRegular,
-    lineHeight: 22,
-    fontSize: FontSize.size_mini,
-    color: Color.colorGray_100,
+  login: {
+    fontSize: 40,
+    color:"#2d2d2d",
+    fontFamily: "Nunito-Bold",
+    marginBottom:"20%",
   },
-  buttonfirstFlexBox: {
-    flexDirection: "row",
-    shadowOpacity: 1,
-    shadowOffset: {
-      width: 0,
-      height: 8,
-    },
-    alignItems: "center",
-  },
-
-  login1: {
-    fontSize: FontSize.size_13xl,
-    color: Color.colorDarkslategray_100,
-    width: 359,
-    textAlign: "center",
-    fontFamily: FontFamily.nunitoBold,
-    fontWeight: "700",
-  },
-  text: {
-    lineHeight: 24,
-    textAlign: "right",
-    color: Color.colorGray_100,
-    left: "0%",
-    top: "0%",
-    height: "100%",
-    position: "absolute",
-    width: "100%",
-  },
-
-  number: {
-    width: 179,
-  },
-  input: {
+  container: {
+    marginBottom:"4%",
     paddingVertical: 16,
     paddingHorizontal: 15,
     width: "75%",
@@ -118,7 +77,7 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: Color.neutralWhite,
+    backgroundColor:"#fff",
     elevation: 30,
     shadowRadius: 30,
     shadowColor: "rgba(80, 85, 136, 0.1)",
@@ -129,75 +88,47 @@ const styles = StyleSheet.create({
     height: 22,
     marginRight: 15,
   },
-  number1: {
-    width: 227,
-  },
-
-  inputs: {
-    width: 291,
-    height: 125,
-  },
   forgotPassword: {
-    width: 299,
-    marginTop: 25,
-    lineHeight: 22,
-    fontSize: FontSize.size_mini,
-    color: Color.colorGray_100,
+    fontSize:15,
+    color: "#7c7c7c",
     textAlign: "center",
-    fontFamily: FontFamily.nunitoBold,
+    fontFamily: "Nunito-Bold",
     fontWeight: "700",
   },
-  signUp: {
-    lineHeight: 24,
-    color: Color.neutralWhite,
-    width: 235,
-    textAlign: "center",
-  },
   buttonfirst: {
-    borderRadius: Border.br_mini,
-    backgroundColor: Color.colorRoyalblue_100,
+    borderRadius: 15,
+    backgroundColor:"#0075fd",
     shadowColor: "rgba(236, 95, 95, 0.25)",
     shadowRadius: 14,
     elevation: 14,
-    width: 317,
+    width: "75%",
     height: 58,
     marginTop: 25,
+    marginBottom:"2%",
     justifyContent: "center",
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    alignItems: "center",
   },
   dontYouHave: {
-    color: Color.colorGray_100,
+    color:  "#7c7c7c",
   },
-  signUp1: {
-    color: Color.colorRoyalblue_100,
+  log: {
+    color: "#0075fd",
+    textDecorationLine:"underline",
   },
-  text1: {
-    fontFamily: FontFamily.nunitoRegular,
-    fontSize: FontSize.subheadLgSHLgMedium_size,
-  },
-  dontYouHaveContainer: {
-    marginTop: 25,
-  },
-  main: {
-    width: 321,
-    height: 290,
-    marginTop: 100,
-    alignItems: "center",
+  text: {
+    fontFamily: "Nunito-Regular",
+    fontSize:16,
   },
   loginpage: {
-    height: 742,
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
-  },
-
-  login: {
-    flex: 1,
-    height: 834,
-    paddingHorizontal: Padding.p_5xs,
-    paddingVertical: 0,
-    alignItems: "center",
-    overflow: "hidden",
-    width: "100%",
-    justifyContent: "space-between",
+    backgroundColor:"#FCFAF6"
   },
 });
 
